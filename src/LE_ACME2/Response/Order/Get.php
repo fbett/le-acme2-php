5db5229787579e8d5ac2e001fc2df90b66e09f85
fbett <info@bett-ingenieure.de>
2021-04-12 15:49:00 +0200

    Revert "Fix removed: not needed anymore"
    This reverts commit 36a8eeecd29b3d230657bc18eea396b25bece0fb.

--- src/LE_ACME2/Response/Order/Get.php
+++ src/LE_ACME2/Response/Order/Get.php
@@ -2,4 +2,23 @@
 
 namespace LE_ACME2\Response\Order;
 
-class Get extends AbstractOrder {}
+use LE_ACME2\Connector\RawResponse;
+use LE_ACME2\Exception;
+
+class Get extends AbstractOrder {
+
+    /**
+     * Get constructor.
+     *
+     * @param RawResponse $raw
+     * @param $orderURL
+     * @throws Exception\InvalidResponse
+     * @throws Exception\RateLimitReached
+     */
+    public function __construct(RawResponse $raw, string $orderURL) {
+
+        parent::__construct($raw);
+
+        $this->_raw->header[] = 'Location: ' . $orderURL; // Dirty fix: Header of response "Get" does not contain an order url, instead of response "Create"
+    }
+}
