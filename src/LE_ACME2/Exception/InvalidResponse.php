9b28e2f6af3143018c1932c6a32dbe40f2b2ade2
fbett <info@bett-ingenieure.de>
2021-05-17 08:07:48 +0200

    Exceptions\InvalidResponse::getRawResponse()

--- src/LE_ACME2/Exception/InvalidResponse.php
+++ src/LE_ACME2/Exception/InvalidResponse.php
@@ -6,7 +6,16 @@ use LE_ACME2\Connector\RawResponse;
 
 class InvalidResponse extends AbstractException {
 
-    public function __construct(RawResponse $raw) {
-        parent::__construct("Invalid response received: " . var_export($raw, true));
+    private $_rawResponse;
+
+    public function __construct(RawResponse $rawResponse) {
+
+        $this->_rawResponse = $rawResponse;
+
+        parent::__construct("Invalid response received: " . var_export($rawResponse, true));
+    }
+
+    public function getRawResponse() : RawResponse {
+        return $this->_rawResponse;
     }
 }
