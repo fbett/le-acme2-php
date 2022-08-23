2031f4a96e07b5ff2e8a0e57d99cd3a5872a6bda
fbett <info@bett-ingenieure.de>
2022-08-23 08:51:08 +0200

    Samples: Reformat code comments

--- Samples/HTTP.php
+++ Samples/HTTP.php
@@ -2,6 +2,10 @@
 
 require_once dirname(dirname(dirname(__FILE__))) . DIRECTORY_SEPARATOR . 'autoload.php'; //Path to composer autoload
 
+/**
+ * CONFIG
+ */
+
 // Config the desired paths
 \LE_ACME2\Account::setCommonKeyDirectoryPath('/etc/ssl/le-storage/');
 \LE_ACME2\Authorizer\HTTP::setDirectoryPath('/var/www/acme-challenges/');
@@ -10,16 +14,34 @@ require_once dirname(dirname(dirname(__FILE__))) . DIRECTORY_SEPARATOR . 'autolo
 \LE_ACME2\Connector\Connector::getInstance()->useStagingServer(true);
 \LE_ACME2\Utilities\Logger::getInstance()->setDesiredLevel(\LE_ACME2\Utilities\Logger::LEVEL_INFO);
 
-// Optional configs
-//\LE_ACME2\Utilities\Certificate::enableFeatureOCSPMustStaple();
-//\LE_ACME2\Order::setPreferredChain(\LE_ACME2\Order::IDENTRUST_ISSUER_CN);
-//\LE_ACME2\Utilities\Event::getInstance()->subscribe(
-//    \LE_ACME2\Utilities\Event::EVENT_CONNECTOR_WILL_REQUEST,
-//    function(string $event, array $payload = null) {
-//       // Do something, f.e. force to save the logs
-//    }
-//);
 
+/**
+ * OPTIONAL CONFIG: Enable feature: OCSP Must Staple
+ *
+ * \LE_ACME2\Utilities\Certificate::enableFeatureOCSPMustStaple();
+ */
+
+/**
+ * OPTIONAL CONFIG: Set preferred chain
+ * It is not reasonable usable in/after 2022, but we keep the method for future situations
+ *
+ * \LE_ACME2\Order::setPreferredChain(\LE_ACME2\Order::IDENTRUST_ISSUER_CN);
+ */
+
+/**
+ * OPTIONAL CONFIG: Event subscriber
+ * 
+ * \LE_ACME2\Utilities\Event::getInstance()->subscribe(
+       \LE_ACME2\Utilities\Event::EVENT_CONNECTOR_WILL_REQUEST,
+       function(string $event, array $payload = null) {
+           // Do something, f.e. force to save the logs
+       }
+   );
+ */
+
+/**
+ * WORKFLOW START
+ */
 
 $account_email = 'test@example.org';
 
@@ -27,12 +49,18 @@ $account = !\LE_ACME2\Account::exists($account_email) ?
     \LE_ACME2\Account::create($account_email) :
     \LE_ACME2\Account::get($account_email);
 
-// Update email address
-// $account->update('new-test@example.org');
+/**
+ * OPTIONAL: Update email address
+ *
+ * $account->update('new-test@example.org');
+ */
 
-// Deactivate account
-// Warning: It seems not possible to reactivate an account.
-// $account->deactivate();
+/**
+ * OPTIONAL: Deactivate account
+ * Warning: It is not possible to reactivate an account.
+ *
+ * $account->deactivate();
+ */
 
 $subjects = [
     'example.org', // First item will be set as common name on the certificate
@@ -48,9 +76,12 @@ if(!\LE_ACME2\Order::exists($account, $subjects)) {
     $order = \LE_ACME2\Order::get($account, $subjects);
 }
 
-// Clear current order (in case to restart on status "invalid")
-// Already received certificate bundles will not be affected
-// $order->clear();
+/**
+ * OPTIONAL: Clear current order (in case to restart on status "invalid")
+ * Already received certificate bundles will not be affected
+ *
+ * $order->clear();
+ */
 
 if($order->shouldStartAuthorization(\LE_ACME2\Order::CHALLENGE_TYPE_HTTP)) {
     // Do some pre-checks, f.e. external dns checks - not required
@@ -81,6 +112,9 @@ if($order->isCertificateBundleAvailable()) {
     $bundle = $order->getCertificateBundle();
     $order->enableAutoRenewal();
 
-    // Revoke certificate
-    // $order->revokeCertificate($reason = 0);
+    /**
+     * OPTIONAL: Revoke certificate
+     *
+     * $order->revokeCertificate($reason = 0);
+     */
 }
